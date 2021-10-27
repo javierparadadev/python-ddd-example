2e8e02133e1cc4a1a90988c7ae7d81825bb931d8
Javier Parada <javierparada73@outlook.com>
2021-10-27 19:00:47 +0200

    Remove credentials from user

--- src/contexts/backoffice/users/domain/entities/User.py
+++ src/contexts/backoffice/users/domain/entities/User.py
@@ -1,6 +1,5 @@
 from typing import Optional, Dict, List
 
-from src.contexts.backoffice.users.domain.entities.UserEmail import UserEmail
 from src.contexts.backoffice.users.domain.entities.UserId import UserId
 from src.contexts.backoffice.users.domain.entities.UserName import UserName
 from src.contexts.shared.domain.value_obj.AggregateRoot import AggregateRoot
@@ -8,14 +7,12 @@ from src.contexts.shared.domain.value_obj.AggregateRoot import AggregateRoot
 
 class User(AggregateRoot):
 
-    def __init__(self, id: UserId, email: UserEmail, name: UserName):
+    def __init__(self, id: UserId, name: UserName):
         self.id = id
-        self.email = email
         self.name = name
 
     def to_primitives(self) -> Optional[Dict, List]:
         return {
             'id': self.id,
-            'email': self.email,
             'name': self.name,
         }
--- src/contexts/backoffice/users/domain/entities/UserEmail.py
+++
@@ -1,7 +0,0 @@
-from src.contexts.shared.domain.value_obj.ValueObject import ValueObject
-
-
-class UserEmail(ValueObject):
-
-    def __init__(self, value: str):
-        super().__init__(value)
