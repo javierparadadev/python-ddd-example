2c9f96dfb054b0c4002f36eaf02fff31845d7bda
Javier Parada <javierparada73@outlook.com>
2021-11-02 17:01:41 +0100

    Add domain events

---
+++ src/contexts/shared/domain/DomainEvent.py
@@ -0,0 +1,27 @@
+import uuid
+from abc import ABC, abstractmethod
+from datetime import datetime
+from typing import Optional
+
+
+class DomainEvent(ABC):
+
+    def __init__(
+            self,
+            name: str,
+            aggregate_id: str,
+            event_id: Optional[str],
+            occurred_on: Optional[datetime]):
+        self.name = name
+        self.aggregate_id = aggregate_id
+        if self.aggregate_id is None:
+            self.aggregate_id = uuid.uuid4()
+        self.id = event_id
+        self.occurred_on = occurred_on
+        self.created_at = datetime.now()
+        if self.occurred_on is None:
+            self.occurred_on = self.created_at
+
+    @abstractmethod
+    def to_primitives(self):
+        raise NotImplementedError()
