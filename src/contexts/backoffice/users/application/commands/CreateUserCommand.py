0a85d4418e389a4412bceea94472587625f48982
Javier Parada <javierparada73@outlook.com>
2021-11-09 17:29:11 +0100

    Add first command

---
+++ src/contexts/backoffice/users/application/commands/CreateUserCommand.py
@@ -0,0 +1,13 @@
+from src.contexts.shared.domain.Command import Command
+
+
+class CreateUserCommand(Command):
+
+    COMMAND_TYPE: str = 'create-user'
+
+    def __init__(self, user_id: str, name: str):
+        self.id = user_id
+        self.name = name
+
+    def get_command_type_name(self) -> str:
+        return self.COMMAND_TYPE
