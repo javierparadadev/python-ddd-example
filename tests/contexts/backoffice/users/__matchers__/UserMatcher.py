5ddac22b3843cf148bab2961cf63fb20c52ec632
Javier Parada <javierparada73@outlook.com>
2021-11-30 16:38:56 +0100

    Add Matchers for test

---
+++ tests/contexts/backoffice/users/__matchers__/UserMatcher.py
@@ -0,0 +1,11 @@
+from src.contexts.backoffice.users.domain.entities.User import User
+
+
+class UserMatcher:
+
+    def __init__(self, user: User):
+        self.user = user
+
+    def __eq__(self, user2: User):
+        user1 = self.user
+        return user1.id.value() == user2.id.value() and user1.name.value() == user2.name.value()
--- tests/contexts/backoffice/users/__mocks__/UserRepositoryMock.py
+++ tests/contexts/backoffice/users/__mocks__/UserRepositoryMock.py
@@ -1,10 +1,11 @@
-from typing import NoReturn, List
+from typing import NoReturn, List, Any
 from unittest.mock import MagicMock
 
 from src.contexts.backoffice.users.domain.UserRepository import UserRepository
 from src.contexts.backoffice.users.domain.entities.User import User
 from src.contexts.shared.domain.BaseObject import BaseObject
 from src.contexts.shared.domain.criteria.Criteria import Criteria
+from tests.contexts.backoffice.users.__matchers__.UserMatcher import UserMatcher
 
 
 class UserRepositoryMock(BaseObject, UserRepository):
@@ -15,8 +16,16 @@ class UserRepositoryMock(BaseObject, UserRepository):
     async def find_by_criteria(self, criteria: Criteria) -> List[User]:
         raise NotImplementedError()
 
-    async def create_one(self, _: User) -> NoReturn:
-        self.__create_one_mock()
+    # ----------------------------------------------------
+    # ------------------ CREATE ONE ----------------------
+    # ----------------------------------------------------
+
+    async def create_one(self, user: User) -> NoReturn:
+        self.__create_one_mock(user)
 
     def assert_create_one_has_been_called(self):
         assert self.__create_one_mock.called
+
+    def assert_create_one_has_been_called_with(self, user: User):
+        user_matcher = UserMatcher(user)
+        assert self.__create_one_mock.assert_called_with(user_matcher)
